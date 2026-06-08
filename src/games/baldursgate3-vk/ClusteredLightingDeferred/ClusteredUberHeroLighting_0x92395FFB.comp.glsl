1ac45cac8c8ba6d239c2df64967aff551c6bcffb
cp-nilly <cp.nilly@gmail.com>
2026-06-08 00:47:43 -0700

    Removed cherry-picked remnants
    Targeting poe2. Removed remnants from cherry-picked commits (bg3, endfield).

--- src/games/baldursgate3-vk/ClusteredLightingDeferred/ClusteredUberHeroLighting_0x92395FFB.comp.glsl
+++
@@ -1,4 +0,0 @@
-#version 450
-#extension GL_GOOGLE_include_directive : require
-#define LAYOUT_GROUP_C
-#include "clustered_uber_body.glsl"
--- src/games/baldursgate3-vk/ClusteredLightingDeferred/ClusteredUberStandard_0xAB2320F6.comp.glsl
+++
@@ -1,4 +0,0 @@
-#version 450
-#extension GL_GOOGLE_include_directive : require
-#define LAYOUT_GROUP_A
-#include "clustered_uber_body.glsl"
